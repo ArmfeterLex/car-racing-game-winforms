bb9aec1a2665507e0ce692e0747aab795d228093
ArmfeterLex <alexromanenkobeats@yandex.ru>
2026-03-01 18:53:14 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace PlayAutoCar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.mashinka2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.babka_porshe = new System.Windows.Forms.PictureBox();
+            this.babka_ferrari = new System.Windows.Forms.PictureBox();
+            this.Doroga2 = new System.Windows.Forms.PictureBox();
+            this.Doroga = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.coin1 = new System.Windows.Forms.PictureBox();
+            this.coin2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mashinka2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.babka_porshe)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.babka_ferrari)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Doroga2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Doroga)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(72)))), ((int)(((byte)(72)))));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(325, 274);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(97, 191);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // mashinka2
+            // 
+            this.mashinka2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(72)))), ((int)(((byte)(72)))));
+            this.mashinka2.Image = ((System.Drawing.Image)(resources.GetObject("mashinka2.Image")));
+            this.mashinka2.Location = new System.Drawing.Point(891, 274);
+            this.mashinka2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.mashinka2.Name = "mashinka2";
+            this.mashinka2.Size = new System.Drawing.Size(100, 191);
+            this.mashinka2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.mashinka2.TabIndex = 1;
+            this.mashinka2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // babka_porshe
+            // 
+            this.babka_porshe.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(72)))), ((int)(((byte)(72)))));
+            this.babka_porshe.Image = ((System.Drawing.Image)(resources.GetObject("babka_porshe.Image")));
+            this.babka_porshe.Location = new System.Drawing.Point(531, 60);
+            this.babka_porshe.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.babka_porshe.Name = "babka_porshe";
+            this.babka_porshe.Size = new System.Drawing.Size(111, 132);
+            this.babka_porshe.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.babka_porshe.TabIndex = 2;
+            this.babka_porshe.TabStop = false;
+            this.babka_porshe.Click += new System.EventHandler(this.babka_porshe_Click);
+            // 
+            // babka_ferrari
+            // 
+            this.babka_ferrari.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(72)))), ((int)(((byte)(72)))));
+            this.babka_ferrari.Image = ((System.Drawing.Image)(resources.GetObject("babka_ferrari.Image")));
+            this.babka_ferrari.Location = new System.Drawing.Point(1045, 60);
+            this.babka_ferrari.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.babka_ferrari.Name = "babka_ferrari";
+            this.babka_ferrari.Size = new System.Drawing.Size(111, 132);
+            this.babka_ferrari.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.babka_ferrari.TabIndex = 3;
+            this.babka_ferrari.TabStop = false;
+            // 
+            // Doroga2
+            // 
+            this.Doroga2.BackColor = System.Drawing.Color.Black;
+            this.Doroga2.Image = ((System.Drawing.Image)(resources.GetObject("Doroga2.Image")));
+            this.Doroga2.Location = new System.Drawing.Point(-1, 0);
+            this.Doroga2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Doroga2.Name = "Doroga2";
+            this.Doroga2.Size = new System.Drawing.Size(1480, 556);
+            this.Doroga2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Doroga2.TabIndex = 4;
+            this.Doroga2.TabStop = false;
+            // 
+            // Doroga
+            // 
+            this.Doroga.Image = ((System.Drawing.Image)(resources.GetObject("Doroga.Image")));
+            this.Doroga.Location = new System.Drawing.Point(0, -556);
+            this.Doroga.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Doroga.Name = "Doroga";
+            this.Doroga.Size = new System.Drawing.Size(1480, 556);
+            this.Doroga.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Doroga.TabIndex = 5;
+            this.Doroga.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(89, 105);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 46);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Red;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(1330, 105);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 46);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "0";
+            // 
+            // coin1
+            // 
+            this.coin1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(72)))), ((int)(((byte)(72)))));
+            this.coin1.Image = ((System.Drawing.Image)(resources.GetObject("coin1.Image")));
+            this.coin1.Location = new System.Drawing.Point(338, 105);
+            this.coin1.Margin = new System.Windows.Forms.Padding(4);
+            this.coin1.Name = "coin1";
+            this.coin1.Size = new System.Drawing.Size(111, 48);
+            this.coin1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.coin1.TabIndex = 8;
+            this.coin1.TabStop = false;
+            // 
+            // coin2
+            // 
+            this.coin2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(72)))), ((int)(((byte)(72)))));
+            this.coin2.Image = ((System.Drawing.Image)(resources.GetObject("coin2.Image")));
+            this.coin2.Location = new System.Drawing.Point(891, 103);
+            this.coin2.Margin = new System.Windows.Forms.Padding(4);
+            this.coin2.Name = "coin2";
+            this.coin2.Size = new System.Drawing.Size(111, 48);
+            this.coin2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.coin2.TabIndex = 9;
+            this.coin2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1469, 554);
+            this.Controls.Add(this.coin2);
+            this.Controls.Add(this.coin1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.babka_porshe);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.mashinka2);
+            this.Controls.Add(this.babka_ferrari);
+            this.Controls.Add(this.Doroga);
+            this.Controls.Add(this.Doroga2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mashinka2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.babka_porshe)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.babka_ferrari)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Doroga2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Doroga)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox mashinka2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox babka_porshe;
+        private System.Windows.Forms.PictureBox babka_ferrari;
+        private System.Windows.Forms.PictureBox Doroga2;
+        private System.Windows.Forms.PictureBox Doroga;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox coin1;
+        private System.Windows.Forms.PictureBox coin2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlayAutoCar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int mousespeed = 25;
+            if (e.KeyData == Keys.A && pictureBox1.Left > 180)
+                pictureBox1.Location = new Point(pictureBox1.Location.X - mousespeed, pictureBox1.Location.Y);
+            if (e.KeyData == Keys.D && pictureBox1.Left < 420)
+                pictureBox1.Location = new Point(pictureBox1.Location.X + mousespeed, pictureBox1.Location.Y);
+            if (e.KeyData == Keys.W)
+                pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - mousespeed);
+            if (e.KeyData == Keys.S)
+                pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + mousespeed);
+            if (e.KeyData == Keys.Left && mashinka2.Left > 600)
+                mashinka2.Location = new Point(mashinka2.Location.X - mousespeed, mashinka2.Location.Y);
+            if (e.KeyData == Keys.Right && mashinka2.Left < 840)
+                mashinka2.Location = new Point(mashinka2.Location.X + mousespeed, mashinka2.Location.Y);
+            if (e.KeyData == Keys.Up)
+                mashinka2.Location = new Point(mashinka2.Location.X, mashinka2.Location.Y - mousespeed);
+            if (e.KeyData == Keys.Down)
+                mashinka2.Location = new Point(mashinka2.Location.X, mashinka2.Location.Y + mousespeed);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (shetchik1 == 30)
+            {
+                timer1.Stop();
+                MessageBox.Show("Порше выиграл!");
+                this.Close();
+            }
+            else if (shetchik2 == 30)
+            {
+                timer1.Stop();
+                MessageBox.Show("Феррари выиграл");
+                this.Close();
+            }
+            int speed = 10;
+            Doroga2.Top += speed;
+            Doroga.Top += speed;
+            if (Doroga2.Top >= 450) { Doroga2.Top = 0; Doroga.Top = -450; }
+            int babkaspeed = 10;
+            babka_ferrari.Top += babkaspeed;
+            babka_porshe.Top += babkaspeed;
+            if (babka_porshe.Top >= 545)
+            {
+                babka_porshe.Top = -100;
+                Random rand = new Random();
+                babka_porshe.Left = rand.Next(180, 450);
+            }
+            if (babka_ferrari.Top >= 545)
+            {
+                babka_ferrari.Top = -100;
+                Random rand1 = new Random();
+                babka_ferrari.Left = rand1.Next(600, 840);
+            }
+
+            if (pictureBox1.Bounds.IntersectsWith(babka_porshe.Bounds))
+            {
+                babka_porshe.Top = -100;
+                Random rand = new Random();
+                babka_porshe.Left = rand.Next(180, 450);
+                shetchik1 = shetchik1 - 1;
+            }
+            else { }
+
+            if (mashinka2.Bounds.IntersectsWith(babka_ferrari.Bounds))
+            {
+                babka_ferrari.Top = -100;
+                Random rand1 = new Random();
+                babka_ferrari.Left = rand1.Next(600, 840);
+                shetchik2 = shetchik2 - 1;
+            }
+            else { }
+            
+            int coinspeed = 10;
+            coin1.Top += coinspeed;
+            coin2.Top += coinspeed;
+            if (coin1.Top >= 545)
+            {
+                coin1.Top = -100;
+                Random rand = new Random();
+                coin1.Left = rand.Next(150, 500);
+            }
+            if (coin2.Top >= 545)
+            {
+                coin2.Top = -100;
+                Random rand1 = new Random();
+                coin2.Left = rand1.Next(550, 900);
+            }
+
+            if (pictureBox1.Bounds.IntersectsWith(coin1.Bounds))
+            {
+                coin1.Top = -100;
+                Random rand = new Random();
+                coin1.Left = rand.Next(180, 450);
+                shetchik1 = shetchik1 + 1;
+                label1.Text = Convert.ToString(shetchik1);
+                
+            }
+            if (mashinka2.Bounds.IntersectsWith(coin2.Bounds))
+            {
+                coin2.Top = -100;
+                Random rand1 = new Random();
+                coin2.Left = rand1.Next(600, 840);
+                shetchik2 = shetchik2 + 1;
+                label2.Text = Convert.ToString(shetchik2);
+                
+                }
+                else { }
+
+
+            }
+            double shetchik1 = 0;
+            double shetchik2 = 0;
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void babka_porshe_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
